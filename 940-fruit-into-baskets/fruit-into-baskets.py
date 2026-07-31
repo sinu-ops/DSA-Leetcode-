9caeb79e0b815e85105af8a16eeed917eb6e554d
sinu-ops <sagarsapkale2005@gmail.com>
2026-07-31 15:54:31 +0530

    Time: 244 ms (80.15%) | Memory: 17.2 MB (5.62%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -6,18 +6,31 @@ class Solution(object):
         res=0
         for R in range(len(fruits)):
             count[fruits[R]] += 1
-            total += 1
+            total+=1
+
+
             while len(count) > 2:
                 f=fruits[L]
                 count[f] -= 1
-                total -= 1
-                L +=1
+                total -=1
+                L+=1
 
                 if not count[f]:
                     count.pop(f)
-            res=max(total,res)
+            res=max(res,total)
         return res
 
+
+
+
+
+
+        
+        
+
+            
+
+ 
 
 
         
