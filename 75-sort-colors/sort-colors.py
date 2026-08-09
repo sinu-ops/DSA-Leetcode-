5bf548958fdad52fd6d10c5115963765eb3de8de
sinu-ops <sagarsapkale2005@gmail.com>
2026-08-09 15:29:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (19.08%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def sortColors(self, nums):
+        n=len(nums)
+        low=0
+        mid=0
+        high=n-1
+        while mid<=high :
+            if nums[mid] == 0:
+                nums[mid], nums[low] = nums[low], nums[mid]
+                low+=1
+                mid +=1
+            elif nums[mid] == 1:
+                mid +=1
+
+            else :
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -=1
+               
+
+        
+     
