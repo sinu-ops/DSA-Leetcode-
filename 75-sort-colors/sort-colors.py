b1a8ca771553c4c6566b208048997eb707ea606e
sinu-ops <sagarsapkale2005@gmail.com>
2026-08-09 15:34:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (56.21%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,17 +4,18 @@ class Solution(object):
         low=0
         mid=0
         high=n-1
-        while mid<=high :
+        while mid <= high :
             if nums[mid] == 0:
-                nums[mid], nums[low] = nums[low], nums[mid]
-                low+=1
+                nums[mid],nums[low] =nums[low] ,nums[mid]
                 mid +=1
-            elif nums[mid] == 1:
+                low +=1
+            elif nums[mid] ==1:
                 mid +=1
-
-            else :
-                nums[mid], nums[high] = nums[high], nums[mid]
+            else:
+                nums[mid],nums[high] = nums[high],nums[mid]
                 high -=1
+
+        
                
 
         
