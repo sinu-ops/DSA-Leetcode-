0c49ba709fb7936abb2b35808f6a43e1b209d60f
sinu-ops <sagarsapkale2005@gmail.com>
2026-08-11 23:10:23 +0530

    Time: 20 ms (41.27%) | Memory: 12.5 MB (99.74%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        L=0
+        R=len(s)-1
+        res=0
+        while L <R:
+            if not s[L].isalnum():
+                L+=1
+                continue
+            if not s[R].isalnum():
+                R-=1
+                continue
+            if s[L].lower() !=s[R].lower():
+                return False
+            L+=1
+            R-=1
+        return True
+
+            
+
+        
+        
+
+       
+        
