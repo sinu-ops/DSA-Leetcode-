e0a2658e8eb0c5b86f7cf10cd4668da635315b37
sinu-ops <sagarsapkale2005@gmail.com>
2026-08-12 11:21:00 +0530

    Time: 3 ms (90.30%) | Memory: 13.6 MB (49.94%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,10 +3,16 @@ class Solution(object):
         new=0
         L=0
         for R in range(len(nums)):
-            if nums[R]:
-                nums[L],nums[R] = nums[R],nums[L]
-                L +=1
+            if nums[R] !=0:
+                nums[L],nums[R]=nums[R],nums[L]
+                L+=1
         return nums
+
+
+        
+      
+            
+      
 
 
 
