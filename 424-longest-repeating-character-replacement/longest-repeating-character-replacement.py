c3f7025d3cb92511662c958cafd5b68255188fc8
sinu-ops <sagarsapkale2005@gmail.com>
2026-08-17 10:32:33 +0530

    Time: 146 ms (45.06%) | Memory: 15.4 MB (61.81%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -5,18 +5,19 @@ class Solution(object):
         freq={}
         max_freq=0
         answer=0
-        
+
         for R in range(len(s)):
-            freq[s[R]] =freq.get(s[R],0)+1
+            freq[s[R]]=freq.get(s[R],0)+1
 
             max_freq=max(max_freq,freq[s[R]])
 
-            while (R-L+1) -max_freq >k:
+            while (R-L+1) - max_freq>k:
                 freq[s[L]] -=1
                 L+=1
-
-            answer=max(answer,R-L+1)
+            answer=max(max_freq,R-L+1)
         return answer
+        
+        
 
         
 
