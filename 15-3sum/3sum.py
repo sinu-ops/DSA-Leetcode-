4728c17f45a094a452206b87eb862bc5113c7646
sinu-ops <sagarsapkale2005@gmail.com>
2026-09-08 17:56:46 +0530

    Time: 751 ms (61.58%) | Memory: 18.3 MB (59.98%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,47 +1,65 @@
 class Solution(object):
     def threeSum(self, nums):
-        class Solution(object):
-            nums.sort()
-
+        nums.sort()
         res = []
         n = len(nums)
+        left=0
+        right=0
 
         for i in range(n):
-
-            # Skip duplicate first values
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i>0 and nums[i] == nums[i-1]:
                 continue
-
-            left = i + 1
-            right = n - 1
-
-            while left < right:
-
-                total = nums[i] + nums[left] + nums[right]
+            left=i+1
+            right=n-1
+            
+            while left < right :
+                total=nums[i] + nums[left] +nums[right]
 
                 if total < 0:
-                    left += 1
-
-                elif total > 0:
-                    right -= 1
-
+                    left +=1
+                elif total >0:
+                    right -=1
                 else:
-                    res.append([nums[i], nums[left], nums[right]])
+                    res.append([nums[i],nums[left],nums[right]])
+                    left +=1
+                    right -=1
 
-                    left += 1
-                    right -= 1
-
-                    # Skip duplicate left values
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-
-                    # Skip duplicate right values
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-
+                    while left <right  and nums[left] == nums[left-1]:
+                        left +=1
+                    while left <right  and nums[right] == nums[right+1]:
+                        right -=1
         return res
-    
+
+
+                   
         
+            
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+            
+                
+                                          
+                    
+             
+            
+
 
         
 
