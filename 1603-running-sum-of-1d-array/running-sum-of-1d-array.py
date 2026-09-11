b18dfd2992622bf1330e64564a9dfc4db318eac8
sinu-ops <sagarsapkale2005@gmail.com>
2026-09-11 23:34:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (96.74%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def runningSum(self, nums):
+        n=len(nums)
+       
+        for i in range(1,n):
+            nums[i]=nums[i] + nums[i-1]
+        return nums
+            
+            
+            
+
+                
+           
+
+        
+           
+                
+
+       
+        
