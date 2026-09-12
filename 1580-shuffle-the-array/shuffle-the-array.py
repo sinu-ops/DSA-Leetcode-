373495c2612b07282dc5f7dabc955c2ac5da6f40
sinu-ops <sagarsapkale2005@gmail.com>
2026-09-12 10:13:59 +0530

    Time: 44 ms (29.32%) | Memory: 12.4 MB (81.99%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        result=[]
+        for i in range(n):
+            result.append(nums[i])
+            result.append(nums[i + n])
+        return result
+            
+
+
+
+            
+
+
+
+
+
+        
+        
